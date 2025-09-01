e9fa64e86add122c00ead2209d19538d3f0d202c
MaizeTheGods <manuelemiliorope@gmail.com>
2025-08-31 18:54:53 -0600

    Update client.js

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  // === ¡AQUÍ ESTÁ EL CAMBIO MÁS IMPORTANTE! ===
-  // Añadimos /api al final de la URL base
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
-
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
